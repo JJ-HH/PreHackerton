ecf862affeb0e6b75d1a5f736157abf9372b5fb2
JJ-HH <docessonhos@naver.com>
2021-05-21 18:52:16 +0900

    last commit

--- WebEDA/homepage/views.py
+++ WebEDA/homepage/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render, HttpResponse, get_object_or_404
 from django.urls import reverse
 from django.views import generic, View
+from django.conf import settings
+
+import os
+import json
 
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        graph = {}
-        return render(request, 'index.html', graph)
+        return render(request, 'index.html', {})
+        # return render(request, 'index.html', {"graph":graph})

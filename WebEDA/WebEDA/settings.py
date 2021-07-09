c322902122262501aa554658de0632f37f7c819d
JJ-HH <docessonhos@naver.com>
2021-07-09 17:00:51 +0900

    Update settings.py

--- WebEDA/WebEDA/settings.py
+++ WebEDA/WebEDA/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@50fj(!asdoxj8k2*u@mu$g$ukve45*40gaui2l+$cab6wbtlx'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
